18490a81d1c0c3cc5c9ea918dd126301efb5d90b
V.H.Nam | apikey:917406166639a085ed6580842d321c43422fc746faba8b96dc56665ce2dfb7cf <vhnam18032002@gmail.com>
2026-04-15 09:14:25 +0700

    up

--- assets/js/config.js
+++ assets/js/config.js
@@ -229,7 +229,7 @@ const CONFIG = {
         }
     },
     ULTIMATE: {
-        MAX_RAGE: 1,      // Giới hạn nộ đầy
+        MAX_RAGE: 100,      // Giới hạn nộ đầy
         GAIN_PER_KILL: 2,   // Lượng nộ nhận được mỗi khi hạ 1 quái
         DURATION_MS: 10000, // Thời gian duy trì trạng thái ultimate
         TRANSITION_MS: 1000, // Thời gian hợp kiếm hoặc tách kiếm
